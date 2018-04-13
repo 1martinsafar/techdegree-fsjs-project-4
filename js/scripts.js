c5cd654b4666f282345e1338225f607a0cab7b64
Martin Safar <1martinsafar@gmail.com>
2018-04-13 12:32:28 +0200

    Basic Game over functionality implemented

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,127 @@ const tickTackToe = () => {
     }
   };
 
+  const boxStatus = box => {
+    if (box.className === "box box-filled-2") {
+      // console.log("Selected: X");
+      return "X";
+    }
+    else if (box.className === "box box-filled-1") {
+      // console.log("Selected: O");
+      return "O";
+    }
+    else {
+      // console.log("empty");
+      return null;
+    }
+  };
+
+  const getRows = () => {
+    console.log(">>> getRows");
+    const allBoxes = Array.from(document.querySelectorAll(".box"));
+    const rows = [];
+    for (let i = 0; i < allBoxes.length; i += 3) {
+      const row = allBoxes.slice(i, i+3);
+      rows.push(row);
+    }
+    return rows;
+  }
+
+  // could take rowLength as a parameter for more reusability
+  const getColumns = () => {
+    console.log(">>> getColumns");
+    const allBoxes = Array.from(document.querySelectorAll(".box"));
+    const columns = [];
+
+    const totalLength = allBoxes.length;
+    const rowLength = 3;
+    const columnCount = totalLength / rowLength;
+    // creating 1 column array element for each row
+    for (let i = 0; i < columnCount; i++) {
+      columns.push([]);
+    }
+    // adding boxes to the appropriate columns
+    for (let i = 0; i < totalLength; i++) {
+      if (i % rowLength === 0) {
+        columns[0].push(allBoxes[i]);
+      }
+      else if (i % rowLength === 1) {
+        columns[1].push(allBoxes[i]);
+      }
+      else {
+        columns[2].push(allBoxes[i]);
+      }
+    }
+    // console.log(columns);
+    return columns;
+  };
+
+  // Assuming the board is a square
+  const getDiagonals = () => {
+    console.log(">>> getDiagonals");
+    const allBoxes = Array.from(document.querySelectorAll(".box"));
+    const diagonals = [[],[]];
+
+    const length = allBoxes.length;
+
+    for (let i = 0; i < length; i++) {
+      // 0 4 8
+      if (i % 4 === 0) {
+        diagonals[0].push(allBoxes[i])
+      }
+      // 2 4 6
+      if (i === 2 || i === 4 || i === 6) {
+        diagonals[1].push(allBoxes[i])
+      }
+    }
+    console.log(diagonals);
+    return diagonals;
+  };
+
+  const checkEnd = directions => {
+    directions.forEach(direction => {
+      const length = direction.length;
+      let same = 0;
+      for (let i = 0; i < length; i++) {
+        if (direction[0].className === "box") {
+          break;
+        }
+        else if (direction[0].className === direction[i].className) {
+          same++;
+        } else {
+          break;
+        }
+        if (same === 3) {
+          console.log("GAME OVER!");
+          const winner = boxStatus(direction[0]);
+          console.log("winner:", winner);
+          return winner;
+        }
+      }
+    });
+  };
+
+  const checkWinRows = rows => {
+    console.log(">>> checkWinRows");
+    checkEnd(rows);
+  };
+
+  const checkWinColumns = columns => {
+    console.log(">>> checkWinColumns");
+    checkEnd(columns);
+  };
+
+  const checkWinDiagonals = diagonals => {
+    console.log(">>> checkWinDiagonals");
+    checkEnd(diagonals);
+  };
+
+  const checkWin = (rows, columns, diagonals) => {
+    checkWinRows(rows);
+    checkWinColumns(columns);
+    checkWinDiagonals(diagonals);
+  };
+
   /* ======================================================================
                           Main Code
   ========================================================================= */
@@ -101,6 +222,21 @@ const tickTackToe = () => {
           currentPlayer = "x";
           showCurrentPlayer();
         }
+        // test winning
+
+        const rows = getRows();
+        const columns = getColumns();
+        const diagonals = getDiagonals();
+        checkWin(rows, columns, diagonals);
+
+        // const rows = getRows();
+        // checkWinRows(rows);
+
+        // const columns = getColumns();
+        // checkWinColumns(columns);
+
+        // const diagonals = getDiagonals();
+        // checkWinDiagonals(diagonals);
       }
     });
   });
