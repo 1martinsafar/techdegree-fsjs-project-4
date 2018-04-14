361ba4a56c17faf888613f441e4f49e40795e150
Martin Safar <1martinsafar@gmail.com>
2018-04-14 18:51:48 +0200

    Implemented a board object handling the display

--- tests/before-html-change-scripts.js
+++ tests/before-html-change-scripts.js
@@ -2,7 +2,7 @@ const tickTackToe = () => {
   "use strict";
 
   /* ======================================================================
-                          FUNCTIONS
+                          Functions
   ========================================================================= */
 
   // Highlighting the user who begins
@@ -154,63 +154,98 @@ const tickTackToe = () => {
   };
 
   /* ======================================================================
-                          OBJECTS
+                          Main Code
   ========================================================================= */
 
   // TESTING BOARD OBJECT
 
   class Board {
+
     showStart() {
-      console.log("___SHOWING START");
-      this.startScreen.style.display = "block";
-      this.endScreen.style.display = "none";
+      console.log("SCREEN before START:\n", this.currentScreen);
+      if (this.currentScreen) {
+        console.log("RESET: START:\n", this.currentScreen);
+        this.currentScreen.style.display = "none";
+        this.screen.insertAdjacentHTML("afterend", this.startScreenHTML);
+        this.currentScreen = this.screen.firstElementChild;
+        return this.screen.innerHTML;
+      } else {
+        this.screen.innerHTML = this.startScreenHTML;
+        this.currentScreen = this.screen.lastElementChild;
+        return this.screen;
+      }
     }
 
-    showBoard(reset) {
-      console.log("___SHOWING BOARD");
-      if (reset) {
-        const boxes = Array.from(document.querySelectorAll(".box"));
-        boxes.forEach(box => {
-          box.className = "box";
-          box.removeAttribute("style");
-        });
-        console.log("boxes:\n", boxes);
-      }
-      this.boardScreen.style.display = "block";
-      this.startScreen.style.display = "none";
-      this.endScreen.style.display = "none";
+    showBoard() {
+      console.log("SCREEN before BOARD:\n", this.currentScreen);
+      this.currentScreen.style.display = "none";
+      console.log("all screen html BEFORE INSERT:\n", this.screen);
+      this.screen.insertAdjacentHTML("afterend", this.boardScreenHTML);
+      console.log("all screen html AFTER INSERT:\n", this.screen);
+      this.currentScreen = this.screen.lastElementChild;
+      return this.screen;
     }
 
     showEnd(resultClass, resultText) {
-      this.endScreen.style.display = "block";
-      this.boardScreen.style.display = "none";
-
-      this.endScreen.className = "screen screen-win";
-      this.endScreen.classList.add(resultClass);
-      const message = document.querySelector("#finish .message");
-      message.textContent = resultText;
+      console.log("SCREEN before BOARD:\n", currentScreen);
+      const screen = document.querySelector("#board");
+      const endScreenHTML = `<div class="screen screen-win ${resultClass}" id="finish">
+        <header>
+          <h1>Tic Tac Toe</h1>
+          <p class="message">${resultText}</p>
+          <a href="#" class="button">New game</a>
+        </header>
+      </div>`;
+      screen.innerHTML = endScreenHTML;
+      return screen.innerHTML;
     }
 
     constructor({  } = {}) {
-      this.startScreen = document.querySelector("#start");
-      this.boardScreen = document.querySelector("#board");
-      this.endScreen = document.querySelector("#finish");
-
-      this.startScreen.style.display = "none";
-      this.boardScreen.style.display = "none";
-      this.endScreen.style.display = "none";
+      // this.screenHTML = screenHTML;
+      this.screen = document.querySelector("#board");
+      this.startScreenHTML = `<div class="screen screen-start" id="start">
+        <header>
+          <h1>Tic Tac Toe</h1>
+          <a href="#" class="button">Start game</a>
+        </header>
+      </div>`;
+      this.boardScreenHTML = `<div class="board" id="board">
+        <header>
+          <h1>Tic Tac Toe</h1>
+          <ul>
+            <li class="players" id="player1"><svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 42 42" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-200.000000, -60.000000)" fill="#000000"><g transform="translate(200.000000, 60.000000)"><path d="M21 36.6L21 36.6C29.6 36.6 36.6 29.6 36.6 21 36.6 12.4 29.6 5.4 21 5.4 12.4 5.4 5.4 12.4 5.4 21 5.4 29.6 12.4 36.6 21 36.6L21 36.6ZM21 42L21 42C9.4 42 0 32.6 0 21 0 9.4 9.4 0 21 0 32.6 0 42 9.4 42 21 42 32.6 32.6 42 21 42L21 42Z"/></g></g></g></svg></li>
+            <li class="players player2" id="player2"><svg xmlns="http://www.w3.org/2000/svg" width="42" height="43" viewBox="0 0 42 43" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-718.000000, -60.000000)" fill="#000000"><g transform="translate(739.500000, 81.500000) rotate(-45.000000) translate(-739.500000, -81.500000) translate(712.000000, 54.000000)"><path d="M30 30.1L30 52.5C30 53.6 29.1 54.5 28 54.5L25.5 54.5C24.4 54.5 23.5 53.6 23.5 52.5L23.5 30.1 2 30.1C0.9 30.1 0 29.2 0 28.1L0 25.6C0 24.5 0.9 23.6 2 23.6L23.5 23.6 23.5 2.1C23.5 1 24.4 0.1 25.5 0.1L28 0.1C29.1 0.1 30 1 30 2.1L30 23.6 52.4 23.6C53.5 23.6 54.4 24.5 54.4 25.6L54.4 28.1C54.4 29.2 53.5 30.1 52.4 30.1L30 30.1Z"/></g></g></g></svg></li>
+          </ul>
+        </header>
+        <ul class="boxes">
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+        </ul>
+      </div>`;
+      this.currentScreen = null;
     }
   }
 
-  /* ======================================================================
-                          Main Code
-  ========================================================================= */
-
   let currentPlayer = "x";
 
-  // const screen = document.querySelector("#board");
+  const screen = document.querySelector("#board");
   let board = new Board({});
   board.showStart();
+  // const screenStart = board.showStart();
+
+  // screenStart.addEventListener("click", e => {
+  //   if (e.target.textContent === "Start game") {
+  //
+  //   }
+  // });
+
 
   const startButton = document.querySelector(".screen-start .button");
   startButton.addEventListener("click", e => {
@@ -263,7 +298,7 @@ const tickTackToe = () => {
           // Restart the game
           const restartButton = document.querySelector("#finish .button");
           restartButton.addEventListener("click", e => {
-            board.showBoard(true);
+            board.showStart();
           });
         } else {
           const rows = getRows();
@@ -282,10 +317,20 @@ const tickTackToe = () => {
             // Restart the game
             const restartButton = document.querySelector("#finish .button");
             restartButton.addEventListener("click", e => {
-              board.showBoard(true);
+              board.showStart();
             });
           }
         }
+
+        // // Restart the game
+        // const restartButton = document.querySelector("#finish .button");
+        // restartButton.addEventListener("click", e => {
+        //   board.showStart();
+        // });
+
+
+
+
       }
     });
   });
